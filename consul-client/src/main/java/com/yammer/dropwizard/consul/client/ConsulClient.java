ac4ae64c118bcc4c9ddf6438f8ab23aefb9d7567
cgray <cgray@yammer-inc.com>
2014-07-28 17:30:46 -0700

    Adding some metrics to the ConsulClient#v1AgentCheckPass

--- consul-client/src/main/java/com/yammer/dropwizard/consul/client/ConsulClient.java
+++ consul-client/src/main/java/com/yammer/dropwizard/consul/client/ConsulClient.java
@@ -1,5 +1,7 @@
 package com.yammer.dropwizard.consul.client;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.google.common.base.Optional;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
@@ -10,32 +12,40 @@ import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 
+import static com.codahale.metrics.MetricRegistry.name;
+
 public class ConsulClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsulClient.class);
     protected static final String v1CheckPassId = "/v1/agent/check/pass";
+    private final Timer v1AgentCheckPassTimer;
     protected final Client client;
     protected final URI consulUri;
 
-    public ConsulClient(Client client, URI consulUri) {
+    public ConsulClient(MetricRegistry metricRegistry,
+                        Client client,
+                        URI consulUri) {
+        this.v1AgentCheckPassTimer = metricRegistry.timer(name(ConsulClient.class, "v1AgentCheckPass"));
         this.client = client;
         this.consulUri = consulUri;
     }
 
     public Optional<ClientResponse> v1AgentCheckPass(String checkId) {
-        ClientResponse clientResponse = null;
-        try {
-            return Optional.of(clientResponse = client.resource(consulUri)
-                    .path(v1CheckPassId)
-                    .path(checkId)
-                    .get(ClientResponse.class));
-        } catch (ClientHandlerException | UniformInterfaceException err) {
-            LOGGER.warn("Exception on {}", v1CheckPassId, err);
-        } finally {
-            if (clientResponse != null) {
-                clientResponse.bufferEntity();
-                clientResponse.close();
+        try (Timer.Context timerContext = v1AgentCheckPassTimer.time()) {
+            ClientResponse clientResponse = null;
+            try {
+                return Optional.of(clientResponse = client.resource(consulUri)
+                        .path(v1CheckPassId)
+                        .path(checkId)
+                        .get(ClientResponse.class));
+            } catch (ClientHandlerException | UniformInterfaceException err) {
+                LOGGER.warn("Exception on {}", v1CheckPassId, err);
+            } finally {
+                if (clientResponse != null) {
+                    clientResponse.bufferEntity();
+                    clientResponse.close();
+                }
             }
+            return Optional.absent();
         }
-        return Optional.absent();
     }
 }
